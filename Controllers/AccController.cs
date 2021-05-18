01c228db8a6d2b98fb347a8c9314d9a7da945ff4
nguyentamthanh <77631594+nguyentamthanh@users.noreply.github.com>
2021-05-18 08:23:10 +0700

    Add project files.

---
+++ Controllers/AccController.cs
@@ -0,0 +1,34 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quanlybanhang.Controllers
+{
+    public class AccController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            
+            if (username == "admin" && password == "admin")
+            {
+                HttpContext.Session.SetString("KeyLogin", "123");
+                return Redirect("/Home/Index");
+            }
+            else
+            {
+                ViewBag.SuccessBody = TempData["successBody"] = "tài khoản hoặc mật khẩu không chính xác !";
+                return Redirect("/Acc/Login");
+            }
+        }
+    }
+}
